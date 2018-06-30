2ed3d543d6049d65705163057fdfac92b8f744aa
hhuynh <hhuynh@connellypartners.com>
2018-06-30 13:18:24 -0400

    Created thumbnail component to encapsulate all of the display logic for a single event. Communication with Child Componets with [event]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { EventsListComponent } from './events-list/events-list.component';
+import { EventsThumbnailComponent } from './events-thumbnail/events-thumbnail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventsThumbnailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/events-thumbnail/events-thumbnail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-events-thumbnail',
+  templateUrl: './events-thumbnail.component.html',
+  styleUrls: ['./events-thumbnail.component.css']
+})
+export class EventsThumbnailComponent implements OnInit {
+
+  @Input() event: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
