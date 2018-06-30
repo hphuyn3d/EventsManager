50941c0e97ffffb9fd44417c26b6647ea31af31a
hhuynh <hhuynh@connellypartners.com>
2018-06-30 13:08:22 -0400

    Added images, and boiler plate for events-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { EventsListComponent } from './events-list/events-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
