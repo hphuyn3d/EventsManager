a1ccacbd3fff5f86585145e180a9cede8b0b150a
Hung Huynh <hung.huynh@payfactors.com>
2018-11-25 20:24:33 -0500

    Reactive Forms (Model Driven Form)

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  profileForm: FormGroup;
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    const firstName = new FormControl(this.authService.currentUser.firstName);
+    const lastName = new FormControl(this.authService.currentUser.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+  cancel() {
+    this.router.navigate(['events']);
   }
 
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+    this.router.navigate(['events'])
+  }
 }
