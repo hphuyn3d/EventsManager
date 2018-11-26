482fc379cd980d76be6dc95a220cb132c7b2de79
Hung Huynh <hung.huynh@payfactors.com>
2018-11-25 19:09:01 -0500

    Validating login

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: ['em { float: right; color:#E05C65; padding-left:10px;}']
 })
 
 export class LoginComponent {
+  mouseoverLogin;
   password;
   userName;
   constructor(private authService: AuthService, private router: Router) {
